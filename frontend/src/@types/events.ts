6e0a56a70e21deebef9837045c3da27ae863019c
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-08-10 02:43:19 -0300

    Feat/frontend (#1)
    * feat(frontend): fazendo setup do next

---
+++ frontend/src/@types/events.ts
@@ -0,0 +1,5 @@
+export interface event{
+  name:string
+  slug:string
+  description:string
+}
---
+++ frontend/src/components/carousel/EventCarousel.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { event } from "@/@types/events";
+import { Carousel,TItemComponent } from "@codelife-ui/react";
+import Link from "next/link";
+const EventItem:TItemComponent<event> = ({description,name,slug}) => {
+
+  return(
+  <Link href={`/events/${slug}`}>
+  <div key={slug}>
+    <h1>{name}</h1>
+    <p>{description}</p>
+  </div>
+  </Link>
+)}
+export default function EventCarousel({events}:{events:event[]}) {
+  return (
+<Carousel<event>
+      ItemComponent={EventItem}
+      items={events}
+      resourceName="events"
+      />
+  );
+}
