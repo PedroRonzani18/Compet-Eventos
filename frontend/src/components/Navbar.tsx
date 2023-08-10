ff221a9cb25382fd5c321f261abc19ce0be22e2f
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-08-10 02:40:45 -0300

    feat(frontend): fazendo setup do next

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { pages } from "@/utils/constants/pages";
+import Link from "next/link";
+interface INavbarProps {
+  children: React.ReactNode;
+}
+function Navbar({children}:INavbarProps) {
+  return ( 
+<>
+    <nav>
+      <ul>
+      {pages.map((page)=>(
+        <li key={page.name}>
+          <Link href={page.url}>
+            {page.name}
+          </Link>
+        </li>
+      ))}
+      </ul>
+    </nav>
+    {children}
+</>
+  );
+}
+
+export default Navbar;
