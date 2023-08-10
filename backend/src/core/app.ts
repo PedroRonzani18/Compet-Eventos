2a19b29d4d64a4c971f251d661dbff6fbdb7c43b
Pedro Augusto de Portilho Ronzani <pedroaugustogabironzani@hotmail.com>
2023-08-10 12:07:56 -0300

    nova branch

--- backend/src/core/app.ts
+++ backend/src/core/app.ts
@@ -1,5 +1,5 @@
 import { projectRoutes } from '@/domain/subdomain/controllers/project/routes'
-import { userRoutes } from '@/domain/subdomain/application/controllers/user/routes'
+import { userRoutes } from '@/domain/subdomain/controllers/user/routes'
 import fastify from 'fastify'
 
 export const app = fastify()
