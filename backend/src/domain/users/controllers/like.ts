82c11fed47a54467eb658194f0745d8577599d1f
Pedro Augusto de Portilho Ronzani <pedroaugustogabironzani@hotmail.com>
2023-08-12 02:01:28 -0300

    Feat(backend): nova rota de usuario favoritar projeto
    refs #7

---
+++ backend/src/domain/users/controllers/like.ts
@@ -0,0 +1,33 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+import { makeFindUserByNameUseCase } from '../usecases/factories/make-find-user-by-name-use-case';
+import { makeLikeProjectUseCase } from '../usecases/factories/make-like-project-use-case';
+
+export async function like(request: FastifyRequest, reply: FastifyReply) { // cria um usuario
+
+	const likeUserBodySchema = z.object({
+		user_name: z.string(),
+		project_name: z.string()
+	});
+
+	const { user_name, project_name } = likeUserBodySchema.parse(request.body);
+
+	const findUserByTitletUseCase = makeFindUserByNameUseCase();
+
+	const user = await findUserByTitletUseCase.execute({ name: user_name })
+
+	if (!user) return reply
+		.status(400)
+		.send({ message: "User not Found for Likeing." })
+
+	const likeUserUseCase = makeLikeProjectUseCase();
+
+	await likeUserUseCase.execute({
+		user_name, project_name
+	});
+
+	return reply
+		.status(201) // retorna sucesso
+		.send();
+}
+
--- backend/src/domain/users/controllers/routes.ts
+++ backend/src/domain/users/controllers/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify';
 import { create } from './create';
 import { edit } from './edit';
+import { like } from './like';
 
 export async function userRoutes(app: FastifyInstance) {
     app.post('/user/create', create)
     app.put('/user/edit', edit)
+    app.put('/user/like', like)
 }
